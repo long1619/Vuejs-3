490102b158de3b5ca11e59a172d344f9424e8d93
duclong1619 <duclong1619@gmail.com>
2024-02-07 07:02:31 +0700

    code vuejs3

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Header />
+    <Todos />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+import Todos from './components/Todos.vue';
+
+export default {
+  name: 'App',
+  components: { Header, Todos }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Courier New', Courier, monospace;
+}
+</style>
---
+++ my-vue-app/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+    <h1>Việc cần làm</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style>
+.header {
+  background: #FFFF99;
+  color: burlywood;
+  text-align: center;
+  padding: 10px;
+}
+</style>
